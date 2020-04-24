11c696e3114a412bf2b10fe0777a1970062474d2
Venkata Vadigepalli <venkata.vadigepalli@playtech.com>
2020-04-24 12:12:21 +0100

    rolled numbers

--- components/HomeComponent.tsx
+++ components/HomeComponent.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import {View, SafeAreaView, StyleSheet, Text, TouchableOpacity, Image} from 'react-native';
+import {View, SafeAreaView, StyleSheet, Text, TouchableOpacity, ImageBackground} from 'react-native';
 import {connect} from 'react-redux';
 import TicketGrid from '../CustomComponents/TicketGrid';
 import TicketsManager from '../managers/ticketsManager';
@@ -50,6 +50,9 @@ class HomeComponent extends Component{
 
     render(){
         const {tickets,numbersRolled} = this.props;
+        let numbersRolledStr = numbersRolled?numbersRolled.toString():"";
+        console.log(numbersRolledStr);
+        let currentRolledNumber = numbersRolled?numbersRolled.slice(-1)[0]:0;
         if(numbersRolled != null && numbersRolled != undefined && numbersRolled.length != 0){
             let number = numbersRolled.slice(-1)[0];
             if(number != this._lastRolledNumber){
@@ -66,9 +69,14 @@ class HomeComponent extends Component{
                         <View style={styles.view}>
                             <Text>Home</Text>
                             <TicketGrid style={{backgroundColor:'red'}} tickets={tickets} onPress={this.onTicketNumberPressed}></TicketGrid>
-                            <Text style={styles.currentRolledNumber}>{numbersRolled?numbersRolled.slice(-1)[0]:0}</Text>
+                            <Text style={{fontSize:20, color:'gray'}}>{numbersRolledStr}</Text>
+                            {/* <Text style={styles.currentRolledNumber}>{currentRolledNumber}</Text> */}
                             <TouchableOpacity style={{justifyContent:'center', alignItems:'center'}} onPress={this.onRollPressed}>
-                                <Image source={require('../images/bingo.png')} style={{width:75, height:75}}></Image>
+                                <ImageBackground source={require('../images/circle-outline.png')} style={{width:100, height:100}}>
+                                <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+                                    <Text style={styles.currentRolledNumber}>{currentRolledNumber}</Text>
+                                </View>
+                                </ImageBackground>
                             </TouchableOpacity>
                         </View>
                     </SafeAreaView>
@@ -96,11 +104,11 @@ const styles = StyleSheet.create({
         fontWeight:'bold'
     },
     currentRolledNumber:{
-        fontSize:75,
+        fontSize:50,
         alignSelf:'center',
         fontWeight:'bold',
-        color:'purple',
-        margin:50
+        color:'red',
+        margin:10
     }
 });
 
